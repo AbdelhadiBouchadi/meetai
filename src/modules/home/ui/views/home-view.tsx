d287b3682b1ca8a9b9e3d62006eb5d3438a48443
Abdelhadi Bouchadi <2110213582@ogrenci.karabuk.edu.tr>
2025-06-10 17:29:08 +0300

    tRPC setup

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,45 +1,16 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
-import { authClient } from '@/lib/auth-client';
-import { useRouter } from 'next/navigation';
+import { useTRPC } from '@/trpc/client';
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 type Props = {};
 
 const HomeView = (props: Props) => {
-  const { data: session } = authClient.useSession();
-  const router = useRouter();
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.hello.queryOptions({ text: 'Abdelhadi' }));
 
-  if (!session) {
-    return (
-      <div className="flex flex-col items-center justify-center h-svh">
-        <p className="text-lg">You are not logged in.</p>
-        <Button asChild>
-          <a href="/sign-in">Sign In</a>
-        </Button>
-      </div>
-    );
-  }
-
-  return (
-    <div className="flex flex-col items-center justify-center h-svh p-4 gap-y-4">
-      <p>
-        {' '}
-        Logged in as{' '}
-        <span className="text-2xl font-bold">{session?.user.name}</span>{' '}
-      </p>
-      <Button
-        onClick={() => {
-          authClient.signOut({
-            fetchOptions: { onSuccess: () => router.push('/sign-in') },
-          });
-        }}
-      >
-        Sign Out
-      </Button>
-    </div>
-  );
+  return <div>{data?.greeting}</div>;
 };
 
 export default HomeView;
