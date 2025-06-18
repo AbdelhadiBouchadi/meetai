e25ca918d7640554f017d8c097e3d4f055b71014
Abdelhadi Bouchadi <2110213582@ogrenci.karabuk.edu.tr>
2025-06-18 16:01:58 +0300

    meetings setup

---
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import ErrorState from "@/components/shared/error-state";
+import LoadingState from "@/components/shared/loading-state";
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import React from "react";
+
+type Props = {};
+
+const MeetingsView = (props: Props) => {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
+
+  return <div>{JSON.stringify(data)}</div>;
+};
+
+export const MeetingsViewLoading = () => {
+  return (
+    <LoadingState
+      title="Loading Meetings"
+      description="This may take a few seconds ..."
+    />
+  );
+};
+
+export const MeetingsViewError = () => {
+  return (
+    <ErrorState
+      title="Error Loading Meetings"
+      description="An Error has occured while loading meetings !"
+    />
+  );
+};
+
+export default MeetingsView;
